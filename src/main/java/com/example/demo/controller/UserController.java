7b1ce4a6d93b2c29fdca7e7458aaed9a6f3f5062
Ivan Matveev <vanadiiii42@gmail.com>
2020-10-24 01:42:54 +0400

    fix UserController

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,22 +1,37 @@
 package com.example.demo.controller;
 
+import com.example.demo.controller.dto.UserDataDto;
+import com.example.demo.controller.dto.UserDto;
+import com.example.demo.controller.mappers.UserDataDtoWebMapper;
+import com.example.demo.controller.mappers.UserWebMapper;
 import com.example.demo.domain.entity.User;
-import com.example.demo.sevice.IUserService;
+import com.example.demo.domain.sevice.IUserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
     private final IUserService userService;
+    private final UserWebMapper userWebMapper;
+    private final UserDataDtoWebMapper userDataDtoWebMapper;
 
-    public User getUser(UUID userId) {
-        return userService.getUser(userId);
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDto> getUser(@PathVariable UUID userId) {
+        return ResponseEntity.ok(userWebMapper.apply(userService.getUser(userId)));
     }
 
-    public void updatePersonalInfo(UUID userId, User user) {
+    @PutMapping("/{userId}")
+    public ResponseEntity<Void> updatePersonalInfo(
+            @PathVariable UUID userId,
+            @RequestBody UserDataDto userDataDto
+    ) {
+        User user = userDataDtoWebMapper.apply(userDataDto);
         userService.updateUser(userId, user);
+        return ResponseEntity.ok().build();
     }
 }
