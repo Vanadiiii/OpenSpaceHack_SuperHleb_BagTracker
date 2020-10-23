b61222407175bdb2b05bcb50d373028b49fa4129
Ivan Matveev <vanadiiii42@gmail.com>
2020-10-24 00:48:18 +0400

    add UserStatistic

---
+++ src/main/java/com/example/demo/domain/entity/UserStatistic.java
@@ -0,0 +1,19 @@
+package com.example.demo.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+
+@Component
+@Data
+@AllArgsConstructor
+@Builder
+public class UserStatistic {
+    private final UUID userId;
+    private List<UUID> bugIdList;
+    private List<UUID> achievementIdList;
+}
