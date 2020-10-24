711aec74f81b7ad84e7b78b979965460594493c7
Ivan Matveev <vanadiiii42@gmail.com>
2020-10-24 16:03:47 +0400

    fix some pair of Dto

--- src/main/java/com/example/demo/web/dto/UserDataDto.java
+++ src/main/java/com/example/demo/web/dto/UserDataDto.java
@@ -1,4 +1,21 @@
 package com.example.demo.web.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Value;
+
+import java.util.UUID;
+
+@Value
+@Builder
+@AllArgsConstructor
 public class UserDataDto {
+    private UUID id;
+    private String mail;
+    private String phone;
+    private String fio;
+    private String login;
+    private String password;
+    private long rating;
+    private long coins;
 }
