1639abc0e97ad1f75afeaa61278fcc66149ef29b
nuod <nuuduha@gmail.com>
2020-10-25 11:47:35 +0400

    add in-mem-db, update README.md

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # OpenSpaceHack_SuperHleb_BagTracker
-команда для запуска приложения, предварительно нужно вставить данные для подключения у БД  
-docker run --net=host -p 8080:8080 -e DB_URL='' -e DB_USER='' -e DB_PWD='' --name bug-repo nuod/open-space-hack_super-hleb_bag-tracker
+после загрузки репозитория запустить 
+mvn clean install
+после запустить сборку контейнера
+docker build -t nuod/open-space-hack_super-hleb_bag-tracker:latest .
+команда для запуска приложения  
+docker run -p 8080:8080 --name bug-repo nuod/open-space-hack_super-hleb_bag-tracker
--- src/main/java/com/example/demo/domain/entity/PlaybackStep.java
+++ src/main/java/com/example/demo/domain/entity/PlaybackStep.java
@@ -5,11 +5,7 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Data
@@ -22,6 +18,7 @@ public class PlaybackStep {
     @GeneratedValue(generator = "uuid2")
     @Type(type = "uuid-char")
     private UUID id;
+    @Column(name = "ordr")
     private Integer order;
     @ManyToOne
     private Bug bug;
